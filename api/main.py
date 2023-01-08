c9eaba66c2c47707d42a585444a4f52dfa6e468e
JC <courier@pop-os.localdomain>
2023-01-08 15:16:17 -0500

    Cleaned stuff

--- api/main.py
+++ api/main.py
@@ -10,16 +10,9 @@ import pandas as pd
  
 app = FastAPI(title="MVP Predictor API", description="API for predicting NBA MVP", version="1.0")
 
-class MVP(BaseModel):
-    data: List[conlist(float, min_items=5, max_items=5)] # VORP WS BPM OWS WS/48
-    #jokic 2022
-    # [ 
-    #   9.8,
-    #   15.2,
-    #   13.7,
-    #   10.8,
-    #   0.296
-    # ]
+
+rf_model = joblib.load("mvp_random_forest.joblib") # ['PTSPG', 'ASTPG', 'WS', 'BLKPG', 'DRBPG', 'VORP', 'BPM', 'USG%', 'FGPG']
+scaled_log_model = joblib.load("mvp_scaled_log.joblib")
 
 # @app.on_event('startup')
 # def load_model():
@@ -84,13 +77,6 @@ async def get_prediction():
     teams = stats_df['Team']
     X = stats_df.drop(columns=['Player', 'Team'])[['PTSPG', 'ASTPG', 'WS', 'BLKPG', 'DRBPG', 'VORP', 'BPM', 'USG%', 'FGPG']]
 
-    rf_model = joblib.load("mvp_random_forest.joblib") # ['PTSPG', 'ASTPG', 'WS', 'BLKPG', 'DRBPG', 'VORP', 'BPM', 'USG%', 'FGPG']
-    scaled_log_model = joblib.load("mvp_scaled_log.joblib")
-    #data = dict(mvp)['data']
-    #print(model)
-    #prediction = rf_model.predict(data).tolist()
-    #log_proba = rf_model.predict_proba(data).tolist()
-
     proba_scaled_log = scaled_log_model.predict_proba(X).tolist()
     proba_RF = rf_model.predict_proba(X).tolist()
     mvp_log_probs = []
@@ -99,15 +85,8 @@ async def get_prediction():
         mvp_log_probs.append(prob[1])
     for prob in proba_RF:
         mvp_rf_probs.append(prob[1])
-    # print(hand_select_models['Scaledlogistic']['fitted_model'].predict_proba(X).tolist())
-    # print(proba_RF)
-    # stats_df['results_scaled_log'] = results_scaled_log
-    # stats_df['results_RF'] = results_RF
+
     stats_df['proba_scaled_log'] = mvp_log_probs
     stats_df['proba_RF'] = mvp_rf_probs
     result_df = stats_df.sort_values(by='proba_RF', ascending=False)[['Player', 'proba_scaled_log', 'proba_RF']].head(10)
     return result_df.to_dict()
-    # return {"prediction": prediction,
-    #         "probs": log_proba,
-    #         "mvp_proba": log_proba[0][1],
-    #         "non_mvp_proba": log_proba[0][0]}
